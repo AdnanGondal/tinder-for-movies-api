9b7bedf947889438340ee87c8a83c5defff26a34
Samuel Tyrrell <Samueltyrrell12@hotmail.com>
2022-02-05 21:26:01 +0000

    connected to the movies database

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const db = require("../models/repo")
+const Movies = require("../services/github")
 
 
 const router = express.Router();
 
 router.get("/", function(req, res) {
-  res.send("Hello World");
+  res.send({'name':"Hello World"});
 });
 
 router.get("/account", async function(req,res){
 
 
   const result = await db.query("SELECT * FROM accounts")
+
   console.log(result.rows)
   res.send(result.rows)
 })
 
+router.get("/movies", async function(req,res){
+ const movies = new Movies()
+ const data = await movies.get()
+
+ res.send(data)
+
+})
+
 module.exports = router;
+
+
 
 
 
